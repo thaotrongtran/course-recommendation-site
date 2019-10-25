1544873f8b3965e4eabeabdd861d6986674d934e
thaotrongtran <ttran88@uncc.edu>
2019-10-25 15:17:45 +0000

    Create db rb added automatically

--- db/migrate/20191024180443_create_reviews.rb
+++ db/migrate/20191024180443_create_reviews.rb
@@ -5,7 +5,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.string :professor
       t.text :evaluation
       t.integer :professor_rating
-      t.integer :workload_rating
+      t.integer :workload_ratingz
       t.integer :support_rating
 
       t.timestamps
