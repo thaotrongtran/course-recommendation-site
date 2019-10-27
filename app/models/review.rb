4aabe2175deb8aff090b51ac00119a2bdc0b0006
Mardi Hach <mhach@uncc.edu>
2019-10-27 00:52:41 +0000

    added simple number rating function to form

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,10 +3,8 @@ class Review < ApplicationRecord
     validates :course_name, presence: true, length: {minimum: 5}
     validates :professor, presence: true, length: {minimum: 5}
     validates :evaluation, presence: true, length: {minimum: 10}
-=begin
     validates :professor_rating, presence: true, length: {minimum: 1}
     validates :workload_rating, presence: true, length: {minimum: 1}
     validates :support_rating, presence: true, length: {minimum: 1}
-=end
     end
 end
