7a723111e84c9347f49c1d8fcbb5000aac03e50e
thaotrongtran <ttran88@uncc.edu>
2019-10-29 17:32:45 +0000

    Forget to save changes so now to save changes again

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,9 +1,4 @@
 class Professor < ApplicationRecord
-<<<<<<< HEAD
-  has_many :reviews
-  has_many :courses
-=======
     has_many :reviews
     has_many :courses
->>>>>>> 2c17a0b
 end
--- db/migrate/20191024180443_create_reviews.rb
+++ db/migrate/20191024180443_create_reviews.rb
@@ -1,11 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-<<<<<<< HEAD
-      t.integer :course_id
-=======
       t.integer:course_id
->>>>>>> 2c17a0b
       t.integer :prof_id
       t.text :evaluation
       t.integer :prof_rating
