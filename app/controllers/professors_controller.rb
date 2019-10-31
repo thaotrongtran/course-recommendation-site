100aa1b223db592a48e131ed09123e3681cc7d9e
thaotrongtran <ttran88@uncc.edu>
2019-10-31 17:47:01 +0000

    Add views for Professors and forms

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,2 +1,36 @@
 class ProfessorsController < ApplicationController
+      #To show all coures
+    def index
+        @professors = Professor.all
+    end
+    
+    #Create new professor
+    def new
+        @professor = Professor.new
+    end 
+    
+    #To create professor and save
+    def create
+        @professor = Professor.new(professor_params)
+        
+        #Adding validation redirection
+        if @professor.save
+            redirect_to @professor
+        else
+            render 'new'
+        end
+    end
+    
+    #To show in professors in a page
+    def show
+        @professor = Professor.find(params[:id])
+    end
+  
 end
+
+
+#For whitelisting
+private
+    def professor_params
+        params.require(:professor).permit(:prof_name)
+    end
