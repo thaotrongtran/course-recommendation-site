d46931f902ff1603bf97d730511abd5e7293048d
thaotrongtran <ttran88@uncc.edu>
2019-10-31 17:06:34 +0000

    Add index method to course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,6 @@
 class CoursesController < ApplicationController
+    #To show all coures
+    def index
+        @courses = Course.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
   
-  resources :reviews
+  #resources :reviews
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
