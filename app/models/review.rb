76f40c5bf99c21d0bf2a67224d5094eaf27d8f94
thaotrongtran <ttran88@uncc.edu>
2019-11-05 18:27:10 +0000

    Show the professor and course name in the review showing page and index page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,10 @@
 class Review < ApplicationRecord
     #belongs_to :course
     #belongs_to :professor
-    validates :course_id, presence: true, length: {minimum: 1}
-    validates :professor_id, presence: true, length: {minimum: 1}
-    validates :evaluation, presence: true, length: {minimum: 10}
-    validates :prof_rating, presence: true, length: {minimum: 1}
-    validates :workload_rating, presence: true, length: {minimum: 1}
-    validates :support_rating, presence: true, length: {minimum: 1}
+    #validates :course_id, presence: true, length: {minimum: 1}
+    #validates :professor_id, presence: true, length: {minimum: 1}
+    #validates :evaluation, presence: true, length: {minimum: 10}
+    #validates :prof_rating, presence: true, length: {minimum: 1}
+    #validates :workload_rating, presence: true, length: {minimum: 1}
+    #validates :support_rating, presence: true, length: {minimum: 1}
 end
