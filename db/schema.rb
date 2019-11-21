e70ae0c2a33c56283ee179ad3f1c19d42d19948c
thaotrongtran <ttran88@uncc.edu>
2019-11-21 18:16:58 +0000

    Output average using Average function of ActiveRecord and drop Calculations model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_184416) do
-
-  create_table "calculations", force: :cascade do |t|
-    t.integer "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_calculations_on_course_id"
-  end
+ActiveRecord::Schema.define(version: 2019_11_21_181534) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
